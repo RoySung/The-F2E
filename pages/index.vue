37ae146af4e26a18eb97d63be35bbe455364b5c9
Roy Cai <roysunghan@gmail.com>
2018-06-11 00:46:47 +0800

    ✨ Feat: clear index page and set router link for todo-List

--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,17 @@
 <template>
   <section class="container">
     <div>
-      <app-logo/>
       <h1 class="title">
-        the-f2e
+        The F2E
       </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
+       <router-link to="/todolist">1. Todo List</router-link>
     </div>
   </section>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
-
 export default {
   components: {
-    AppLogo
   }
 }
 </script>
