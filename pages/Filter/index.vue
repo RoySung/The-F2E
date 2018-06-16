1f5b3f8f2719d306ab221c82723d75cc226fe4f2
Roy Cai <roysunghan@gmail.com>
2018-06-16 18:40:19 +0800

    ✨ Feat: add Filter page

---
+++ pages/Filter/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="filter-page">
+    Filter
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Filter'
+  }
+</script>
+
+<style lang="sass" src="./style.sass">
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,48 +1,54 @@
 <template>
   <section class="container">
-    <div>
-      <h1 class="title">
-        The F2E
-      </h1>
-       <router-link to="/todolist">1. Todo List</router-link>
+    <h1 class="title">
+      The F2E
+    </h1>
+    <div class="links-list">
+      <router-link to="/todolist">1. Todo List</router-link>
+      <router-link to="/filter">1. Filter</router-link>
     </div>
   </section>
 </template>
 
 <script>
 export default {
-  components: {
-  }
+  components: {}
 }
 </script>
 
-<style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+<style lang="sass">
+.container
+  min-height: 100vh
+  display: flex
+  justify-content: center
+  align-items: center
+  text-align: center
+  flex-direction: column
 
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
+.title
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif /* 1 */
+  display: block
+  font-weight: 300
+  font-size: 100px
+  color: #35495e
+  letter-spacing: 1px
 
-.links {
-  padding-top: 15px;
-}
+
+.subtitle
+  font-weight: 300
+  font-size: 42px
+  color: #526488
+  word-spacing: 5px
+  padding-bottom: 15px
+
+
+.links-list
+  padding-top: 15px
+  display: flex
+  flex-direction: column
+  a
+    text-align: left
+
+
 </style>
