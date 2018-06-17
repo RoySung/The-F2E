6d8a527b5d0e1edc24fe17c4ed236792baddf89c
Roy Cai <roysunghan@gmail.com>
2018-06-18 01:52:44 +0800

    ✨ Feat: handle search-wrap UI With tags and search-wrap

--- pages/Filter/index.vue
+++ pages/Filter/index.vue
@@ -1,12 +1,46 @@
 <template>
   <div class="filter-page">
-    Filter
+    <div :class="{ 'search-wrap--searched': isSearched }" class="search-wrap">
+      <h1 class="search-title">The F2E</h1>
+      <div class="search-input-wrap">
+        <input class="search-input" type="text" v-moel="searchText" @keyup.enter="search">
+        <i class="fas fa-search" @click="search"></i>
+      </div>
+      <div id="search-btn" @click="search">Search</div>
+    </div>
+    <div v-if="isSearched" class="content">
+      <Tags class="stages-wrap" title="Stages" :options="stages" v-model="selectedStage"></Tags>
+      <Tags class="tags-wrap" title="Tags" :options="tags" v-model="selectedTags"></Tags>
+    </div>
+
   </div>
 </template>
 
 <script>
+  import Tags from './Tags'
+  import { stages, tags } from './constants'
+
   export default {
-    name: 'Filter'
+    name: 'Filter',
+    data() {
+      return {
+        searchText: null,
+        isSearched: false,
+        stages,
+        tags,
+        selectedStage: [],
+        selectedTags: []
+      }
+    },
+    methods: {
+      search() {
+        console.log('search')
+        this.isSearched = true
+      }
+    },
+    components: {
+      Tags
+    }
   }
 </script>
 
