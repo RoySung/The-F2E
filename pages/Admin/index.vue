2daa4574cea500cdeb556ea9b5d32b43c407d14b
Roy Cai <roysunghan@gmail.com>
2018-06-21 17:51:21 +0800

    ✨ Feat: handle UI of the header

--- pages/Admin/index.vue
+++ pages/Admin/index.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="admin-page">
-    Admin
+    <div class="header">
+      <div class="header__logo-wrap">
+        <h3 class="logo-text">Shop Admin</h3>
+      </div>
+      <div class="header__navigation-bar">
+        <div class="navigation-tabs">
+          <div class="tab">Home</div>
+          <div class="tab">Orders</div>
+          <div class="tab">Product</div>
+        </div>
+      </div>
+      <div class="header__user-wrap">Admin</div>
+    </div>
   </div>
 </template>
 
@@ -10,6 +22,5 @@
   }
 </script>
 
-<style scoped>
-
+<style lang="sass" src="./style.sass">
 </style>
