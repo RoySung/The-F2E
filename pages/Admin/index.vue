e159437ca079737ec1c0f2b53d4d62b2114e99d9
Roy Cai <roysunghan@gmail.com>
2018-06-22 00:53:51 +0800

    ✨ Feat: separate header styele and handle overview-wrap with total-wrap

--- pages/Admin/index.vue
+++ pages/Admin/index.vue
@@ -13,6 +13,26 @@
       </div>
       <div class="header__user-wrap">Admin</div>
     </div>
+    <div class="overview-wrap">
+      <h3 class="title-wrap">Overview</h3>
+      <div class="filter-wrap">filter</div>
+      <div class="card total-view total-revenue-wrap">
+        <h4 class="total-view__title"><i class="fas fa-hand-holding-usd"></i>TOTAL REVENUE</h4>
+        <div class="total-view__count">54,540</div>
+      </div>
+      <div class="card total-view total-cost-wrap">
+        <h4 class="total-view__title"><i class="fas fa-money-check-alt"></i>TOTAL COST</h4>
+        <div class="total-view__count">12,660</div>
+      </div>
+      <div class="card total-view net-income-wrap">
+        <h4 class="total-view__title"><i class="fas fa-money-bill"></i>NET INCOME</h4>
+        <div class="total-view__count">41,880</div>
+      </div>
+
+      <div class="card analytics-chart">chart</div>
+      <div class="card transaction-website">transaction</div>
+      <div class="card latest-orders">orders</div>
+    </div>
   </div>
 </template>
 
