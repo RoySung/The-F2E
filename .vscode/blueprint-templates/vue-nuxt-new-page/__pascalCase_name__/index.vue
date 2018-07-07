752aa39ef92450d0dc6a4ce7ad6e50e4aa5f62f1
Roy Cai <roysunghan@gmail.com>
2018-07-07 22:30:00 +0800

    ✨ Feat: add vscode config about blueprint with page template

---
+++ .vscode/blueprint-templates/vue-nuxt-new-page/__pascalCase_name__/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="{{kebabCase name}}-page">
+    {{camelCase name}}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "{{pascalCase name}}"
+  }
+</script>
+
+<style lang="sass" src="./style.sass" scoped>
+
+</style>
