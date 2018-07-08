1008865e951e57ae20fe8616ca5938e7c1024e50
Roy Cai <roysunghan@gmail.com>
2018-07-08 15:16:26 +0800

    ✨ Feat: handle selection-bar with selection and theme toggler in the chapter page

--- pages/ComicViewer.vue
+++ pages/ComicViewer.vue
@@ -3,42 +3,56 @@
     <div class="header header--bright">
       <h1 class="header-title">Comicomic</h1>
     </div>
-    <div class="info-wrap">
-      <div class="info-wrap__image">
-        <img src="@/static/comic/comic_cover.png" alt="">
-      </div>
-      <div class="info-wrap__content">
-        <div class="info-wrap__content__title">
-          <h2>MY HEXSCHOOL</h2>
+    <div v-if="!isReadingPage" class="home-page">
+      <div class="info-wrap">
+        <div class="info-wrap__image">
+          <img src="@/static/comic/comic_cover.png" alt="">
         </div>
-        <ul class="info-wrap__content__info">
-          <li class="info-field info-genres"><span>Genres</span>Fusce/vehicula/dolor</li>
-          <li class="info-field info-author"><span>Author</span>Namae Shiranai</li>
-          <li class="info-field info-status"><span>Status</span>Ongoing</li>
-          <li class="info-field info-rate"><span>Rate</span><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i></li>
-          <li class="info-summary">
-            <span>Summary</span>
-            <p>If your banker breaks, you snap; if your apothecary by mistake sends you poison in your pills, you die.<br><br>Therefore, I say, I saw that this situation of mine was the precise situation of every mortal that has this Siamese connexion with a plurality of other mortals. </p>
-          </li>
-        </ul>
+        <div class="info-wrap__content">
+          <div class="info-wrap__content__title">
+            <h2>MY HEXSCHOOL</h2>
+          </div>
+          <ul class="info-wrap__content__info">
+            <li class="info-field info-genres"><span>Genres</span>Fusce/vehicula/dolor</li>
+            <li class="info-field info-author"><span>Author</span>Namae Shiranai</li>
+            <li class="info-field info-status"><span>Status</span>Ongoing</li>
+            <li class="info-field info-rate"><span>Rate</span><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i></li>
+            <li class="info-summary">
+              <span>Summary</span>
+              <p>If your banker breaks, you snap; if your apothecary by mistake sends you poison in your pills, you die.<br><br>Therefore, I say, I saw that this situation of mine was the precise situation of every mortal that has this Siamese connexion with a plurality of other mortals. </p>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="chapters-wrap">
+        <div class="chapters-wrap__title">
+          <h3>All Chapters</h3>
+        </div>
+        <div class="chapters-wrap__content">
+          <p @click="goChapter(0)">Chapter 1: The F2E Challenge Start!</p>
+          <p @click="goChapter(1)">Chapter 2: Todo List is Going Crazy! <span class="new-tag">New</span></p>
+        </div>
       </div>
     </div>
-    <div class="chapters-wrap">
-      <div class="chapters-wrap__title">
-        <h3>All Chapters</h3>
-      </div>
-      <div class="chapters-wrap__content">
-        <p>Chapter 1: The F2E Challenge Start!</p>
-        <p>Chapter 2: Todo List is Going Crazy! <span class="new-tag">New</span></p>
-      </div>
-    </div>
-    <nuxt-child />
+    <nuxt-child v-else/>
   </div>
 </template>
 
 <script>
   export default {
-    name: "ComicViewer"
+    name: "ComicViewer",
+    computed: {
+      isReadingPage() {
+        return this.$route.params.chapter != null
+      }
+    },
+    methods: {
+      goChapter(index) {
+        let params = this.$route.params
+        params.chapter = index
+        this.$router.push({ params })
+      }
+    }
   }
 </script>
 
--- pages/ComicViewer/_chapter.vue
+++ pages/ComicViewer/_chapter.vue
@@ -1,15 +1,64 @@
 <template>
-  <div>
+  <div :class="{ 'dark-theme': isDarkTheme }" class="reading-page">
+    <div class="selection-bar">
+      <label class="selection-bar__name">My Hexschool<i class="fas fa-caret-right"></i></label>
+      <select type="text" class="selection-bar__chapter-select">
+        <option value="1">Chapter 1</option>
+        <option value="2">Chapter 2</option>
+      </select>
+      <select type="text" class="selection-bar__page-select">
+        <option value="1">Chapter 1</option>
+        <option value="2">Chapter 2</option>
+      </select>
+      <div class="selection-bar__toggle-wrap">
+        <i class="fas fa-sun"></i>
+        <input v-model="isDarkTheme" type="checkbox" id="dark-theme-toggler" name="theme-toggler">
+        <label for="dark-theme-toggler" class="input-slider"></label>
+        <i v-if="isDarkTheme" class="fas fa-moon"></i>
+        <i v-else class="far fa-moon"></i>
+      </div>
+
+    </div>
+
     {{ $route.params.chapter }}
+
   </div>
 </template>
 
 <script>
   export default {
-    name: "Chapter"
+    name: "Chapter",
+    data() {
+      return {
+        isDarkTheme: false,
+        chatpersArr: [
+          {
+            name: 'Chatper 1',
+            content: [
+              'https://hexschool.github.io/THE_F2E_Design/week5-comic%20viewer/assets/storyboard-1.png',
+              'https://hexschool.github.io/THE_F2E_Design/week5-comic%20viewer/assets/storyboard-2.png',
+              'https://hexschool.github.io/THE_F2E_Design/week5-comic%20viewer/assets/storyboard-3.png',
+              'https://hexschool.github.io/THE_F2E_Design/week5-comic%20viewer/assets/storyboard-4.png',
+              'https://hexschool.github.io/THE_F2E_Design/week5-comic%20viewer/assets/storyboard-5.png',
+              'https://hexschool.github.io/THE_F2E_Design/week5-comic%20viewer/assets/storyboard-6.png',
+              'https://hexschool.github.io/THE_F2E_Design/week5-comic%20viewer/assets/storyboard-7.png',
+              'https://hexschool.github.io/THE_F2E_Design/week5-comic%20viewer/assets/storyboard-8.png',
+              'https://hexschool.github.io/THE_F2E_Design/week5-comic%20viewer/assets/storyboard-9.png',
+              'https://hexschool.github.io/THE_F2E_Design/week5-comic%20viewer/assets/storyboard-10.png',
+              'https://hexschool.github.io/THE_F2E_Design/week5-comic%20viewer/assets/storyboard-11.png',
+              'https://hexschool.github.io/THE_F2E_Design/week5-comic%20viewer/assets/storyboard-12.png',
+
+            ]
+          }
+        ],
+        chatpterIndex: 0
+      }
+    },
+    computed: {
+    }
   }
 </script>
 
-<style scoped>
+<style lang="sass" src="./chapter.sass" scoped>
 
 </style>
